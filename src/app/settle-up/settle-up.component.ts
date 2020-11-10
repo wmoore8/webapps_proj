73cd55d3c9da2dc165ea852d92b64fbaad334bea
mike-maloney87 <71612972+mike-maloney87@users.noreply.github.com>
2020-11-09 21:17:56 -0600

    added paypal to settle-up component

--- src/app/settle-up/settle-up.component.ts
+++ src/app/settle-up/settle-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-settle-up',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settle-up.component.css']
 })
 export class SettleUpComponent implements OnInit {
+  public newValue = '0';
+  public displayMainContent;
+  public displayContent;
+  public displayName;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.displayMainContent=true;
+    this.displayContent=false;
+    this.displayName=false;
   }
+
+  public addValue() {
+    this.displayMainContent=false;
+    this.displayContent=true;
+   }
+
+   public display(){
+      this.displayName = true;
+   }
+
 
 }
